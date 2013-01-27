fb6393f4b9a425aaaa4a8f2ccbb4100d27c244f9
Joshua Tacoma <joshua@yellowseed.org>
2013-01-27 11:50:37 -0500

    Drop json: prefix in encoding tags.
    This will make the tags work better across other encodings (JSON is not
    intended to be the only supported encoding).

--- zdcf1.go
+++ zdcf1.go
@@ -7,43 +7,43 @@ import (
 )
 
 type zdcf1 struct {
-	Version float32          `json:"version"`
-	Apps    map[string]*app1 `json:"apps"`
+	Version float32          `version`
+	Apps    map[string]*app1 `apps`
 }
 
 type app1 struct {
-	Context *context1           `json:"context"`
-	Devices map[string]*device1 `json:"devices"`
+	Context *context1           `context`
+	Devices map[string]*device1 `devices`
 }
 
 type context1 struct {
-	IoThreads int  `json:"iothreads"`
-	Verbose   bool `json:"verbose"`
+	IoThreads int  `iothreads`
+	Verbose   bool `verbose`
 }
 
 type device1 struct {
-	Type    string              `json:"type"`
-	Sockets map[string]*socket1 `json:"sockets"`
+	Type    string              `type`
+	Sockets map[string]*socket1 `sockets`
 }
 
 type socket1 struct {
-	Type    string    `json:"type"`
-	Options *options1 `json:"option"`
-	Bind    []string  `json:"bind"`
-	Connect []string  `json:"bind"`
+	Type    string    `type`
+	Options *options1 `option`
+	Bind    []string  `bind`
+	Connect []string  `bind`
 }
 
 type options1 struct {
-	Hwm         int      `json:"hwm"`
-	Swap        int      `json:"swap"`
-	Affinity    int      `json:"affinity"`
-	Identity    string   `json:"identity"`
-	Subscribe   []string `json:"subscribe"`
-	Rate        int      `json:"rate"`
-	RecoveryIvl int      `json:"recovery_ivl"`
-	McastLoop   bool     `json:"mcast_loop"`
-	SndBuf      int      `json:"sndbuf"`
-	RcvBuf      int      `json:"rcvbuf"`
+	Hwm         int      `hwm`
+	Swap        int      `swap`
+	Affinity    int      `affinity`
+	Identity    string   `identity`
+	Subscribe   []string `subscribe`
+	Rate        int      `rate`
+	RecoveryIvl int      `recovery_ivl`
+	McastLoop   bool     `mcast_loop`
+	SndBuf      int      `sndbuf`
+	RcvBuf      int      `rcvbuf`
 }
 
 func unmarshalZdcf1(bytes []byte) (*zdcf1, error) {
